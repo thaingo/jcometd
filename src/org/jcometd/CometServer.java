85cc4a06b1c0bbda8da60b015accc5429884f29d
Zeran <zeran@mail.ua>
2012-06-05 00:31:32 +0300

    test

---
+++ src/org/jcometd/CometServer.java
@@ -0,0 +1,17 @@
+package org.jcometd;
+
+public class CometServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public CometServer() {
+		
+	}
+
+}
